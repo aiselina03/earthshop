0e315e43165ef1b56f9197cdaa69e2647033844f
Aysel <146956428+aiselina03@users.noreply.github.com>
2023-11-16 11:53:06 +0400

    Add files via upload

---
+++ EarthAll/assets/js/shop.js
@@ -0,0 +1,16 @@
+const bars = document.querySelector(".fa-bars");
+const xmark = document.querySelector(".fa-xmark");
+const menushow = document.querySelector(".menushow");
+
+bars.addEventListener("click", function () {
+    bars.classList.add("d-none")
+    menushow.classList.remove("d-none")
+    xmark.classList.remove("d-none")
+    console.log(bars);
+})
+
+xmark.addEventListener("click", function () {
+    menushow.classList.add("d-none")
+    xmark.classList.add("d-none")
+    bars.classList.remove("d-none")
+})
